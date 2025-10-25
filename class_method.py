aa49d4e460ec6a28b0015ece021e8b8f8e217022
Methminpulsara <methminpulsara10@gmail.com>
2025-10-25 11:07:35 +0530

    update

--- class_method.py
+++ class_method.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
 from typing import ClassVar
 
+
 @dataclass
-
-class_v = 10
-
 class Student:
-    school : ClassVar[str] # this also class veriable
-    name : str
-    age : int
+    school: ClassVar[str]  # this  class variable
+    name: str #thease are class object variables
+    age: int
+
+    def print_student_info(self):  #self tag eken refer krnne class eken hadhana object ekea meeka object eke method ekk
+        print(f"name {self.name}")
